a255440be553761b7b4f8ed299ddaf92c0a60c9d
BHushanKothawade2004 <bhushankothawade2004@gmail.com>
2024-04-19 19:12:52 +0530

    Introducing Error Handling

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
-const port = 8080; 
 const path = require("path");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const port = 8080; 
 
 app.engine("ejs", ejsMate);    
 
@@ -43,56 +44,60 @@ app.get("/", (req, res) => {
 //     await sampleListing.save();
 //     console.log("Sample was saved");
 //     res.send("successful testing");
-// })
+// });
 
 //Index Route
 app.get("/listings", async (req, res) => {
     const allListing = await Listing.find({});
     res.render("listings/index.ejs", { allListing }); 
-})
+});
 
 //Create Route
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync (async (req, res, next) => {
     //method 1 to extract data (usual method)
     // let {title, description, price, country, location} = req.body
     //method 2 create object listing in ejs file and then use instance of that object
     const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-})
+     await newListing.save();
+     res.redirect("/listings");
+}));
 
 // Edit Route
 app.get("/listings/:id/edit", async (req, res) => { 
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", {listing});
-})
+});
 
 //Update Route
 app.put("/listings/:id", async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing}) 
     res.redirect("/listings");
-})
+});
 
 app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs")
-})
+});
 
 //Show Route
 app.get("/listings/:id", async (req, res) => { 
     let {id} = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-})
+    res.render("listings/show.ejs", {listing}); 
+});
 
 app.delete("/listings/:id", async (req, res) => {
     let {id} = req.params;
     let deleteListing = await Listing.findByIdAndDelete(id);
     console.log(deleteListing);
     res.redirect("/listings");
-})
+});
+
+app.use((err, req, res, next) => {
+    res.send("Something went wrong");
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
-});
+}); 
